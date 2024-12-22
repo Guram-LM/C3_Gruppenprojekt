41b7ffc66e30b2a13e608a8665430cef6704cbbf
TamunaVepkhvadze <tamunavepkhvade@gmail.com>
2024-12-22 23:26:43 +0400

    Add data faile

---
+++ data.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+// ავტორი თამუნა ვეფხვაძე
+// ჰოსპიტალის შესახებ 
+$about_hospital = [
+    "title" => "About Hospital",
+    "highlight" => "Hospital",
+    "description" => "has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors.",
+    "read_more_link" => "#"
+];
+
+// ავტორი თამუნა ვეფხვაძე
+// ჰოსპიტალის შესახებ 
