20e0d33fbd7a518fd3467acb172d067fdb180887
TamunaVepkhvadze <tamunavepkhvade@gmail.com>
2024-12-22 23:40:19 +0400

    hospital-treatment

--- data.php
+++ data.php
@@ -13,3 +13,35 @@ $about_hospital = [
 
 // ავტორი თამუნა ვეფხვაძე
 // ჰოსპიტალის შესახებ 
+
+
+// ავტორი თამუნა ვეფხვაძე
+// ჰოსპიტალური მლურნალობა
+$Cure=[
+    [
+    'img' => 'images/t1.png',
+    'title' => 'Nephrologist Care',
+    'description' => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    'Learn more' => 'Read More',
+    ],
+    [
+    'img' => 'images/t2.png',
+    'title' => 'Eye Care',
+     'description' => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    'Learn more' => 'Read More',
+    ],
+    [
+    'img' => 'images/t3.png',
+    'title' => 'Pediatrician Clinic',
+    'description' => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    'Learn more' => 'Read More',
+    ],
+    [
+    'img' => 'images/t4.png',
+    'title' => 'Parental Care',
+    'description' => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+    'Learn more' => 'Read More'
+    ],
+];
+// ავტორი თამუნა ვეფხვაძე
+// ჰოსპიტალური მლურნალობა
