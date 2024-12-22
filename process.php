59a4554d0dd30b7d0f037fc7914bb99f41943a94
Guram_L <lomadzeg@gmail.com>
2024-12-22 14:28:02 +0100

    new_css_style

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/new_style.css">
     <title>Thank You</title>
 </head>
 <?php
