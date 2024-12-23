b2b61be73f4aa27fd9ef71e83d9a69b2d760e7b4
Guram_L <lomadzeg@gmail.com>
2024-12-23 12:50:41 +0100

    Add contact page

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!-- კონტაქტის ველი 
+    ავტორი გურამ ლომაძე -->
+<html>
+
+<?php
+include './component/header.php';
+
+?>
+
+
+<body class="sub_page">
+
+<?php
+include './component/contact.php';
+
+include "./component/footer.php";
+
+?>
+
+</body>
+
+</html>
+<!-- კონტაქტის ველი 
+    ავტორი გურამ ლომაძე -->
